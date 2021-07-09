07d1e57ebef235990d01bd61687f11a400d390fe
Lee Cullip <lcullip@gmail.com>
2021-07-09 22:25:18 +0100

    More Fixes

--- TechSwap/settings.py
+++ TechSwap/settings.py
@@ -13,11 +13,11 @@ SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = (
+ALLOWED_HOSTS = [
     "127.0.0.1",
     ".herokuapp.com",
     "glacial-sierra-78162.herokuapp.com",
-)
+]
 
 
 INSTALLED_APPS = [
