bb143176008dcef1a0f7fe3e8b573b8e9eb38097
Lee Cullip <lcullip@gmail.com>
2021-07-10 00:35:47 +0100

    Fixed

--- TechSwap/urls.py
+++ TechSwap/urls.py
@@ -11,7 +11,9 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += path("__debug__/", include(debug_toolbar.urls))
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
 
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
